069116f02380e85c487bb10a33f861de2b78ade8
Koen Vendrik <k.vendrik@gmail.com>
2015-01-20 14:03:35 +0100

    initial commit

---
+++ challenger.class.rb
@@ -0,0 +1,51 @@
+class Challenger
+
+  def initialize(options)
+
+    # read a file
+    def read_file(file_name)
+      file = File.open(file_name, "r")
+      data = file.read
+      file.close
+      return data
+    end
+
+    # open frameworks and challengers files
+    frameworksInfo = JSON.parse(read_file("data/frameworks.json"))
+    challenges = JSON.parse(read_file("data/challenges.json"))
+
+    challengeDetails = {}
+
+    # get language to use
+    inputMatch = frameworksInfo.select { |key, value| key.downcase == options[:language] }
+    if inputMatch.keys.size > 0
+      challengeDetails[:language] = inputMatch.keys[0]
+    else
+      challengeDetails[:language] = frameworksInfo.keys[rand(frameworksInfo.keys.size)]
+    end
+
+    # get array of frameworks
+    frameworks = frameworksInfo[challengeDetails[:language]].keys
+
+    # get framework to use
+    challengeDetails[:framework] = frameworks[rand(frameworks.size)]
+    challengeDetails[:frameworkDetails] = frameworksInfo[challengeDetails[:language]][challengeDetails[:framework]]
+
+    # get the challenge
+    challengeDetails[:challengeDetails] = challenges.values[rand(challenges.values.size)]
+
+    # save to class
+    @challengeDetails = challengeDetails;
+
+  end
+
+  def construct_str()
+    challengeDetails = @challengeDetails
+    framework = challengeDetails[:framework]
+    frameworkDetails = challengeDetails[:frameworkDetails]
+    language = challengeDetails[:language]
+    challengeDetails = challengeDetails[:challengeDetails]
+    return "#{challengeDetails['description']} in #{language} with #{framework}.\n#{frameworkDetails['homepage']}\n#{challengeDetails['homepage']}"
+  end
+
+end
